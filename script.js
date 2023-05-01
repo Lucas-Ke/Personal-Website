6311918b66df003969745ea7de55101059161c16
Lucas-Ke <116187385+Lucas-Ke@users.noreply.github.com>
2023-05-01 10:44:58 -0700

    asfdasd

--- script.js
+++ script.js
@@ -27,10 +27,11 @@ down.addEventListener ("click", ()=>{
 
 const area = ['0', 'space1', 'space2', 'space3', 'space4', 'space5', 'space6', 'space7', 'space8', 'space9', 'space10', 'space11', 'space12', 'space13', 'space14', 'space15', 'space16', 'space17', 'space18', 'space19', 'space20', 'space21', 'space22', 'space23', 'space24', 'space25', 'space26', 'space27', 'space28', 'space29', 'space30', 'space31', 'space32', 'space33', 'space34', 'space35', 'space36', 'space37', 'space38', 'space39', 'space40', 'space41', 'space42', 'space43', 'space44', 'space45', 'space46', 'space47', 'space48', 'space49', 'space50', 'space51', 'space52', 'space53', 'space54', 'space55', 'space56', 'space57', 'space58', 'space59', 'space60', 'space61', 'space62', 'space63', 'space64', 'space65', 'space66', 'space67', 'space68', 'space69', 'space70', 'space71', 'space72', 'space73', 'space74', 'space75', 'space76', 'space77', 'space78', 'space79', 'space80', 'space81', 'space82', 'space83', 'space84', 'space85', 'space86', 'space87', 'space88', 'space89', 'space90', 'space91', 'space92', 'space93', 'space94', 'space95', 'space96', 'space97', 'space98', 'space99', 'space100', 'space101', 'space102', 'space103', 'space104', 'space105', 'space106', 'space107', 'space108', 'space109', 'space110', 'space111', 'space112', 'space113', 'space114', 'space115', 'space116', 'space117', 'space118', 'space119', 'space120', 'space121']
 
+var ShipStop = 0
 var local = 61;
 function MoveUp() {
     console.log("up")
-    if (local == 1 || local == 2 || local == 3 || local == 4 || local == 5 || local == 6 || local == 7 || local == 8 || local == 9 || local == 10 || local == 11) {
+    if (ShipStop == 1 || local == 1 || local == 2 || local == 3 || local == 4 || local == 5 || local == 6 || local == 7 || local == 8 || local == 9 || local == 10 || local == 11) {
     } else {
         const node = document.getElementById(area[local]).lastElementChild;
         local -= 11;
@@ -40,7 +41,7 @@ function MoveUp() {
 }
 function MoveLeft() {
     console.log("left")
-    if (local == 1 || local == 12 || local == 23 || local == 34 || local == 45 || local == 56 || local == 67 || local == 78 || local == 89 || local == 100 || local == 111) {
+    if (ShipStop == 1 || local == 1 || local == 12 || local == 23 || local == 34 || local == 45 || local == 56 || local == 67 || local == 78 || local == 89 || local == 100 || local == 111) {
     } else {
     const node = document.getElementById(area[local]).lastElementChild;
     local -= 1;
@@ -50,7 +51,7 @@ function MoveLeft() {
 }
 function MoveRight() {
     console.log("right")
-    if (local == 11 || local == 22 || local == 33 || local == 44 || local == 55 || local == 66 || local == 77 || local == 88 || local == 99 || local == 110 || local == 121) {
+    if (ShipStop == 1 || local == 11 || local == 22 || local == 33 || local == 44 || local == 55 || local == 66 || local == 77 || local == 88 || local == 99 || local == 110 || local == 121) {
     } else {
     const node = document.getElementById(area[local]).lastElementChild;
     local += 1;
@@ -60,7 +61,7 @@ function MoveRight() {
 }
 function MoveDown() {
     console.log("down")
-    if (local == 111 || local == 112 || local == 113 || local == 114 || local == 115 || local == 116 || local == 117 || local == 118 || local == 119 || local == 120 || local == 121) {
+    if (ShipStop == 1 || local == 111 || local == 112 || local == 113 || local == 114 || local == 115 || local == 116 || local == 117 || local == 118 || local == 119 || local == 120 || local == 121) {
     } else {
     const node = document.getElementById(area[local]).lastElementChild;
     local += 11;
@@ -76,17 +77,21 @@ function SelectPlan() {
     } else if (local == 25) {
         document.getElementById('textbox').innerHTML = 'Here is planet of icecubed';
         document.getElementById('box').classList.add('IceLand')
+        HideShip()
     } else if (local == 31) {
         document.getElementById('textbox').innerHTML = 'Here is planet of mars';
     } else if (local == 57) {
         document.getElementById('textbox').innerHTML = 'Here is planet of Event Horizen';
         document.getElementById('box').classList.add('SunLand')
+        HideShip()
     } else if (local == 65) {
         document.getElementById('textbox').innerHTML = 'Here is planet of Puk-Mine';
         document.getElementById('box').classList.add('PacLand')
+        HideShip()
     } else if (local == 91) {
         document.getElementById('textbox').innerHTML = 'Here is planet of Urple Ring';
         document.getElementById('box').classList.add('RingLand')
+        HideShip()
     } else if (local == 97) {
         document.getElementById('textbox').innerHTML = 'Here is planet of Robotic Star';
     } else if (local == 105) {
@@ -100,7 +105,8 @@ function HideShip(){
     const node = document.getElementById(area[local]).lastElementChild;
     local = 61;
     document.getElementById(area[local]).appendChild(node);  
-    document.getElementById('space61')
+    ShipStop = 1
+    document.getElementById(area[local]).style.visibility = "hidden"
 }
 
 let RedButtA = document.querySelector(".RedButtA")
@@ -115,6 +121,8 @@ RedButtB.addEventListener ("click", ()=>{
     document.getElementById('box').classList.remove('SunLand')
     document.getElementById('box').classList.remove('PacLand')
     document.getElementById('box').classList.remove('RingLand')
+    ShipStop = 0
+    document.getElementById(area[local]).style.visibility = "visible"
 }
 )
 function DisplayA() {
