4e37086c1d34797de8f822cec42e3d939da875dd
Lucas-Ke <116187385+Lucas-Ke@users.noreply.github.com>
2023-05-05 10:43:18 -0700

    Added a star trail to the cursor

--- script.js
+++ script.js
@@ -82,28 +82,28 @@ function SelectPlan() {
         document.getElementById('textbox').innerHTML = 'Here is planet of colors ';
         document.getElementById('box').classList.add('ColorLand')
         HideShip()
-    } else if (local == 25) {
+    } else if (local == 46) {
         document.getElementById('textbox').innerHTML = 'Here is planet of icecubed';
         document.getElementById('box').classList.add('IceLand')
         HideShip()
-    } else if (local == 31) {
-        document.getElementById('textbox').innerHTML = 'Here is planet of mars';
-    } else if (local == 57) {
+    // } else if (local == 31) {
+    //     document.getElementById('textbox').innerHTML = 'Here is planet of mars';
+    } else if (local == 54) {
         document.getElementById('textbox').innerHTML = 'Here is planet of Event Horizen';
         document.getElementById('box').classList.add('SunLand')
         HideShip()
-    } else if (local == 65) {
-        document.getElementById('textbox').innerHTML = 'Here is planet of Puk-Mine';
-        document.getElementById('box').classList.add('PacLand')
-        HideShip()
-    } else if (local == 91) {
+    } else if (local == 92) {
         document.getElementById('textbox').innerHTML = 'Here is planet of Urple Ring';
         document.getElementById('box').classList.add('RingLand')
         HideShip()
-    } else if (local == 97) {
-        document.getElementById('textbox').innerHTML = 'Here is planet of Robotic Star';
-    } else if (local == 105) {
-        document.getElementById('textbox').innerHTML = 'Here is planet of Target Practice';
+    } else if (local == 96) {
+        document.getElementById('textbox').innerHTML = 'Here is planet of Puk-Mine';
+        document.getElementById('box').classList.add('PacLand')
+        HideShip()
+    // } else if (local == 97) {
+    //     document.getElementById('textbox').innerHTML = 'Here is planet of Robotic Star';
+    // } else if (local == 105) {
+    //     document.getElementById('textbox').innerHTML = 'Here is planet of Target Practice';
     } else {
 document.getElementById('textbox').innerHTML = 'Land on a planet first';
     }
@@ -139,4 +139,70 @@ function DisplayA() {
 function DisplayB() {
     document.getElementById('textbox').innerHTML = 'The "B" button was pressed';
 }
+
+var dots = [],
+    mouse = {
+      x: 0,
+      y: 0
+    };
+
+// The Dot object used to scaffold the dots
+var Dot = function() {
+  this.x = 0;
+  this.y = 0;
+  this.node = (function(){
+    var n = document.createElement("div");
+    n.className = "trail";
+    document.body.appendChild(n);
+    return n;
+  }());
+};
+// The Dot.prototype.draw() method sets the position of 
+  // the object's <div> node
+Dot.prototype.draw = function() {
+  this.node.style.left = this.x + "px";
+  this.node.style.top = this.y + "px";
+};
+
+// Creates the Dot objects, populates the dots array
+for (var i = 0; i < 10; i++) {
+  var d = new Dot();
+  dots.push(d);
+}
+
+// This is the screen redraw function
+function draw() {
+  // Make sure the mouse position is set everytime
+    // draw() is called.
+  var x = mouse.x,
+      y = mouse.y;
+  
+  // This loop is where all the 90s magic happens
+  dots.forEach(function(dot, index, dots) {
+    var nextDot = dots[index + 1] || dots[0];
+    
+    dot.x = x - 4;
+    dot.y = y - 4;
+    dot.draw();
+    x += (nextDot.x - dot.x) * .3;
+    y += (nextDot.y - dot.y) * .3;
+
+  });
+}
+
+addEventListener("mousemove", function(event) {
+  //event.preventDefault();
+  mouse.x = event.pageX;
+  mouse.y = event.pageY;
+});
+
+// animate() calls draw() then recursively calls itself
+  // everytime the screen repaints via requestAnimationFrame().
+function animate() {
+  draw();
+  requestAnimationFrame(animate);
+}
+
+// And get it started by calling animate().
+animate();
 
