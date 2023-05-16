4ce32991f31773bd3f0af7e4e6275cde6f4ee98a
Lucas-Ke <116187385+Lucas-Ke@users.noreply.github.com>
2023-05-16 10:43:04 -0700

    asd

--- help.js
+++ help.js
@@ -1,68 +1,36 @@
-// dots is an array of Dot objects,
-// mouse is an object used to track the X and Y position
-   // of the mouse, set with a mousemove event listener below
-   var dots = [],
-   mouse = {
-     x: 0,
-     y: 0
-   };
+const images = ['url(BOcube.png)', 'url(BOcolor.png)', 'url(BOhole.png)']
+var image2 = 1
+var image1 = 2
+var wait = 0
 
-// The Dot object used to scaffold the dots
-var Dot = function() {
- this.x = 0;
- this.y = 0;
- this.node = (function(){
-   var n = document.createElement("div");
-   n.className = "trail";
-   document.body.appendChild(n);
-   return n;
- }());
-};
-// The Dot.prototype.draw() method sets the position of 
- // the object's <div> node
-Dot.prototype.draw = function() {
- this.node.style.left = this.x + "px";
- this.node.style.top = this.y + "px";
-};
+for (let i = 0; i == wait;){
+  wait = 1
+  setTimeout(Moved, 3000)
+  setTimeout(rewait, 3000)
+}  
 
-// Creates the Dot objects, populates the dots array
-// for (var i = 0; i < 12; i++) {
- var d = new Dot();
- dots.push(d);
-// }
-
-// This is the screen redraw function
-function draw() {
- // Make sure the mouse position is set everytime
-   // draw() is called.
- var x = mouse.x,
-     y = mouse.y;
- 
- // This loop is where all the 90s magic happens
- dots.forEach(function(dot, index, dots) {
-   var nextDot = dots[index + 1] || dots[0];
-   
-   dot.x = x - 12;
-   dot.y = y - 12;
-   dot.draw();
-   x += (nextDot.x - dot.x) * .3;
-   y += (nextDot.y - dot.y) * .3;
-
- });
+function rewait(){
+  wait = 0
 }
 
-addEventListener("mousemove", function(event) {
- //event.preventDefault();
- mouse.x = event.pageX;
- mouse.y = event.pageY;
-});
-
-// animate() calls draw() then recursively calls itself
- // everytime the screen repaints via requestAnimationFrame().
-function animate() {
- draw();
- requestAnimationFrame(animate);
+function Moved() {
+  document.getElementById('one-box').classList.add('one--move')
+  document.getElementById('two-box').classList.add('two--move')
+  if (image2 >= 3){
+    image2 = 0
+  }
+  if (image1 >= 3){
+    image1 = 0
+  }
+  setTimeout (Return, 500)
 }
 
-// And get it started by calling animate().
-animate();
+function Return() {
+  console.log('hello')
+  document.getElementById('one-box').classList.remove('one--move')
+  document.getElementById('two-box').classList.remove('two--move')
+  document.getElementById("one-box").style.backgroundImage=images[image1]
+  document.getElementById("two-box").style.backgroundImage=images[image2]
+  image2 += 1
+  image1 += 1  
+}
