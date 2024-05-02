255208b6ffc1ff5f155301b48551a4901f0c6dce
Lucas-Ke <116187385+Lucas-Ke@users.noreply.github.com>
2024-05-01 20:35:34 -0700

    a

--- help.js
+++ help.js
@@ -1,36 +1,36 @@
-const images = ['url(BOcube.png)', 'url(BOcolor.png)', 'url(BOhole.png)']
-var image2 = 1
-var image1 = 2
-var wait = 0
+// const images = ['url(BOcube.png)', 'url(BOcolor.png)', 'url(BOhole.png)']
+// var image2 = 1
+// var image1 = 2
+// var wait = 0
 
-for (let i = 0; i == wait;){
-  wait = 1
-  // setTimeout(Moved, 3000)
-  setTimeout(rewait, 3000)
-}  
+// for (let i = 0; i == wait;){
+//   wait = 1
+//   // setTimeout(Moved, 3000)
+//   setTimeout(rewait, 3000)
+// }  
 
-function rewait(){
-  wait = 0
-}
+// function rewait(){
+//   wait = 0
+// }
 
-function Moved() {
-  document.getElementById('one-box').classList.add('one--move')
-  document.getElementById('two-box').classList.add('two--move')
-  if (image2 >= 3){
-    image2 = 0
-  }
-  if (image1 >= 3){
-    image1 = 0
-  }
-  setTimeout (Return, 500)
-}
+// function Moved() {
+//   document.getElementById('one-box').classList.add('one--move')
+//   document.getElementById('two-box').classList.add('two--move')
+//   if (image2 >= 3){
+//     image2 = 0
+//   }
+//   if (image1 >= 3){
+//     image1 = 0
+//   }
+//   setTimeout (Return, 500)
+// }
 
-function Return() {
-  console.log('hello')
-  document.getElementById('one-box').classList.remove('one--move')
-  document.getElementById('two-box').classList.remove('two--move')
-  document.getElementById("one-box").style.backgroundImage=images[image1]
-  document.getElementById("two-box").style.backgroundImage=images[image2]
-  image2 += 1
-  image1 += 1  
-}
+// function Return() {
+//   console.log('hello')
+//   document.getElementById('one-box').classList.remove('one--move')
+//   document.getElementById('two-box').classList.remove('two--move')
+//   document.getElementById("one-box").style.backgroundImage=images[image1]
+//   document.getElementById("two-box").style.backgroundImage=images[image2]
+//   image2 += 1
+//   image1 += 1  
+// }
